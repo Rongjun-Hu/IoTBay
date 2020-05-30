a393db29358203aa69739eb7d1912d5875cee8ec
STKKKKK <43334005+STKKKKK@users.noreply.github.com>
2020-05-30 17:37:45 +0800

    Update customerTable.sql

--- web/db/customerTable.sql
+++ web/db/customerTable.sql
@@ -4,12 +4,23 @@
  * Created: 12/05/2020
  */
 
-create table CUSTOMER
+-- drop table customer;
+
+CREATE TABLE USERS
 (
 Email varchar(50),
 Name varchar(10),
 Password varchar(20),
 Gender varchar(10),
 Favcol varchar(10),
+DOB date,
 PRIMARY KEY(Email));
+
+INSERT INTO USERS (EMAIL, NAME, PASSWORD, GENDER, FAVCOL, DOB)
+VALUES
+('steve@uts.edu.au', 'Hong Kung', 'a12431868', 'male', 'purple', '1997-04-08');
+
+select * from USERS;
+
+
 
