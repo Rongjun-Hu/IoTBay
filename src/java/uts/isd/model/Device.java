ece035cd628d4c4e827a933c32066dc0231dd90d
Boon <62001580+Boonnapoj@users.noreply.github.com>
2020-06-01 19:52:36 +1000

    Update Device.java
    Device bean uploaded

--- src/java/uts/isd/model/Device.java
+++ src/java/uts/isd/model/Device.java
@@ -5,10 +5,69 @@
  */
 package uts.isd.model;
 
+import java.io.Serializable;
+
 /**
  *
- * @author apple
+ * @author diamo
  */
-public class Device {
+public class Device implements Serializable {
+
+    private int id;
+    private String name;
+    private double price;
+    private String description;
+    private int stock
+
+    public Device(int id, String name, double price, String description, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
     
+    public void setStock(Int stock){
+        this.stock = stock;
+    }
+    
+    public void setDevice(int id, String name, double price, String description, int stock){
+        setId(id);
+        setName(name);
+        setPrice(price);
+        setDescription(description);
+        setStock(stock);
+    }
+
 }
