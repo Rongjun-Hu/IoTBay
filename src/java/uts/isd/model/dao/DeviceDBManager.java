bc972b14401096cab8f3212898f2d0ef22572c1b
Boon <62001580+Boonnapoj@users.noreply.github.com>
2020-06-09 08:26:46 +1000

    dbmanager

--- src/java/uts/isd/model/dao/DeviceDBManager.java
+++ src/java/uts/isd/model/dao/DeviceDBManager.java
@@ -6,23 +6,23 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import uts.isd.model.Device;
+
 /**
  *
  * @author apple
  */
 public class DeviceDBManager {
-    
+
     private Statement st;
 
     public DeviceDBManager(Connection conn) throws SQLException {
         st = conn.createStatement();
     }
 
-    public void createDevice(String name, double price, String type, int stock) throws SQLException {                    
+    public void createDevice(String name, double price, String type, int stock) throws SQLException {
         st.executeUpdate("INSERT INTO IOTUSER.DEVICES(NAME, PRICE, TYPE, STOCK) VALUES('" + name + "'," + price + ",'" + type + "'," + stock + ")");
     }
-    
-    
+
     public ArrayList<Device> findDevices(String name, String type) throws SQLException {
 
         String fetch = "SELECT * FROM IOTUSER.DEVICES WHERE NAME ='" + name + "'" + "AND TYPE ='" + type + "'";
@@ -35,7 +35,7 @@ public class DeviceDBManager {
         }
         ResultSet rs = st.executeQuery(fetch);
         ArrayList<Device> temp = new ArrayList();
-        
+
         while (rs.next()) {
             int id = rs.getInt(1);
             String nam = rs.getString(2);
@@ -46,29 +46,39 @@ public class DeviceDBManager {
         }
         return temp;
     }
-    
-//    public Device findDevice(String id) throws SQLException {
-//        String fetch = "SELECT * FROM IOTUSER.DEVICES WHERE ID='" + id + "'";
-//        ResultSet rs = st.executeQuery(fetch);
-//        
-//        while (rs.next()) {
-//            String name = rs.getString(2);
-//            double price = rs.getDouble(3);
-//            String type = rs.getString(4);
-//            int stock = rs.getInt(5);
-//            return new Device(id, name, price, type, stock);
-//        }
-//        return null;
-//    }
-    
-    public void updateDevice(int id, String name, double price, String type, int stock) throws SQLException { 
-        st.executeUpdate("UPDATE IOTUSER.DEVICES SET NAME ='" +name+ "',PRICE =" +price+ ",TYPE ='" +type+ "',STOCK =" +stock+ "WHERE ID =" +id);
+
+    public Device findDevice(String id) throws SQLException {
+        int deviceId = Integer.parseInt(id);
+        String fetch = "SELECT * FROM IOTUSER.DEVICES WHERE ID=" + deviceId;
+        ResultSet rs = st.executeQuery(fetch);
+       
+
+        while (rs.next()) {
+            String name = rs.getString(2);
+            double price = rs.getDouble(3);
+            String type = rs.getString(4);
+            int stock = rs.getInt(5);
+            return new Device(deviceId, name, price, type, stock);
+        }
+        return null;
+    }
+
+    public void updateDevice(int id, String name, double price, String type, int stock) throws SQLException {
+        st.executeUpdate("UPDATE IOTUSER.DEVICES SET NAME ='" + name + "',PRICE =" + price + ",TYPE ='" + type + "',STOCK =" + stock + "WHERE ID =" + id);
     }
 
     public void deleteDevice(int id) throws SQLException {
         st.executeUpdate("DELETE FROM IOTUSER.DEVICES WHERE ID =" + id);
     }
     
+     public void updateStock(int id, int amount) throws SQLException {
+         String iD = String.valueOf(id);
+         Device device = findDevice(iD);
+         int stock = device.getStock();
+         stock = stock - amount;
+        st.executeUpdate("UPDATE IOTUSER.DEVICES SET STOCK =" + stock + "WHERE ID =" + id);
+    }
+
     public boolean idExist(int id) throws SQLException {
         ResultSet rs = st.executeQuery("SELECT ID FROM DEVICES");
         while (rs.next()) {
@@ -78,5 +88,5 @@ public class DeviceDBManager {
         }
         return false;
     }
-    
+
 }
--- src/java/uts/isd/model/dao/OrderDBManager.java
+++ src/java/uts/isd/model/dao/OrderDBManager.java
@@ -17,4 +17,108 @@ public class OrderDBManager {
     public OrderDBManager(Connection conn) throws SQLException {
         st = conn.createStatement();
     }
+    
+    public Order createOrder(String email, int deviceId, String orderDate, int amount, int status) throws SQLException {                    
+        st.executeUpdate("INSERT INTO IOTUSER.ORDERS(EMAIL, DEVICEID, ORDERDATE, AMOUNT, STATUS) VALUES('" + email + "'," + deviceId + ",'" + orderDate + "'," + amount + "," + status + ")");
+        String fetch = "SELECT MAX(ORDERID) FROM IOTUSER.ORDERS";
+        ResultSet rs = st.executeQuery(fetch);
+         while (rs.next()) {
+            int orderId = rs.getInt(1);
+            return new Order(orderId, email, deviceId, orderDate, amount, status);
+        }
+        return null;
+    }
+    
+    
+    public ArrayList<Order> findOrders(String order, String eMail, String orderdate, int orderstatus) throws SQLException {
+        int orderID = 1;
+        if (!order.equals("")){
+            orderID = Integer.parseInt(order);}
+        else {
+            orderID = 0;
+        }
+        if (orderstatus == 0){
+        String fetch = "SELECT * FROM IOTUSER.ORDERS WHERE ORDERID =" + orderID + " AND ORDERDATE ='" + orderdate + "' AND STATUS =" + orderstatus;
+        if (order.equals("") && orderdate.equals("")) {
+            fetch = "SELECT * FROM IOTUSER.ORDERS WHERE EMAIL ='" + eMail + "' AND STATUS =" + orderstatus;
+        }
+        else if (!order.equals("") && orderdate.equals("")){
+            fetch = "SELECT * FROM IOTUSER.ORDERS WHERE ORDERID =" + orderID + " AND STATUS =" + orderstatus; 
+        }
+        else if (order.equals("") && !orderdate.equals("")){
+            fetch = "SELECT * FROM IOTUSER.ORDERS WHERE ORDERID ='" + orderdate + "' AND STATUS =" + orderstatus;  
+        } 
+        ResultSet rs = st.executeQuery(fetch);
+        ArrayList<Order> temp = new ArrayList();
+        
+        while (rs.next()) {
+            int orderId = rs.getInt(1);
+            String email = rs.getString(2);
+            int deviceId = rs.getInt(3);
+            String orderDate = rs.getString(4);
+            int amount = rs.getInt(5);
+            int status = rs.getInt(6);
+            temp.add(new Order(orderId, email, deviceId, orderDate, amount, status));
+        }
+        return temp;
+        } else { 
+            String fetch = "SELECT * FROM IOTUSER.ORDERS WHERE ORDERID =" + orderID + " AND ORDERDATE ='" + orderdate + "' AND STATUS =" + orderstatus;
+        if (order.equals("") && orderdate.equals("")) {
+            fetch = "SELECT * FROM IOTUSER.ORDERS  WHERE EMAIL ='" + eMail + "' AND STATUS =" + orderstatus;
+        }
+        else if (!order.equals("") && orderdate.equals("")){
+            fetch = "SELECT * FROM IOTUSER.ORDERS WHERE ORDERID =" + orderID + " AND STATUS =" + orderstatus; 
+        }
+        else if (order.equals("") && !orderdate.equals("")){
+            fetch = "SELECT * FROM IOTUSER.ORDERS WHERE ORDERID ='" + orderdate + "' AND STATUS =" + orderstatus;  
+        } 
+        ResultSet rs = st.executeQuery(fetch);
+        ArrayList<Order> temp = new ArrayList();
+        
+        while (rs.next()) {
+            int orderId = rs.getInt(1);
+            String email = rs.getString(2);
+            int deviceId = rs.getInt(3);
+            String orderDate = rs.getString(4);
+            int amount = rs.getInt(5);
+            int status = rs.getInt(6);
+            temp.add(new Order(orderId, email, deviceId, orderDate, amount, status));
+        }
+        return temp;
+    }
+    }
+     public Order findOrder(int orderID) throws SQLException {
+        String order = Integer.toString(orderID);
+        String fetch = "SELECT * FROM IOTUSER.ORDERS WHERE ORDERID =" + orderID;
+        ResultSet rs = st.executeQuery(fetch);
+         while (rs.next()) {
+            int orderId = rs.getInt(1);
+            String email = rs.getString(2);
+            int deviceId = rs.getInt(3);
+            String orderDate = rs.getString(4);
+            int amount = rs.getInt(5);
+            int status = rs.getInt(6);
+            return new Order(orderId, email, deviceId, orderDate, amount, status);
+        }
+        return null;
+     }
+    
+    public void updateOrder(int orderId, String email, String orderDate, int amount, int status) throws SQLException { 
+        st.executeUpdate("UPDATE IOTUSER.ORDERS SET EMAIL ='" + email + "',ORDERDATE ='" + orderDate + "',AMOUNT =" + amount + ",STATUS ="  + status + " WHERE ORDERID =" +orderId);
+    }
+
+    public void deleteOrder(int orderId) throws SQLException {
+        int status = 4;
+        st.executeUpdate("UPDATE FROM IOTUSER.ORDERS SET STATUS=" + status + " WHERE ORDERID =" + orderId);
+    }
+    
+    public boolean orderIdExist(int orderId) throws SQLException {
+        ResultSet rs = st.executeQuery("SELECT ORDERID FROM ORDERS");
+        while (rs.next()) {
+            if (orderId == rs.getInt(1)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
