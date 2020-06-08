f8d6c00a73ca2f541beb564e394901ef4a13abd2
Boon <62001580+Boonnapoj@users.noreply.github.com>
2020-06-09 08:25:11 +1000

    order

--- src/java/uts/isd/model/Order.java
+++ src/java/uts/isd/model/Order.java
@@ -5,10 +5,78 @@
  */
 package uts.isd.model;
 
+import java.io.Serializable;
+
 /**
  *
- * @author apple
+ * @author diamo
  */
-public class Order {
-    
+public class Order implements Serializable {
+
+    private int orderId;
+    private String email;
+    private int deviceId;
+    private String orderDate;
+    private int amount;
+    private int status;
+
+    public Order(int orderId, String email, int deviceId, String orderDate, int amount, int status) {
+        this.orderId = orderId;
+        this.email = email;
+        this.deviceId = deviceId;
+        this.orderDate = orderDate;
+        this.amount = amount;
+        this.status = status;
+
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(int deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+
 }
