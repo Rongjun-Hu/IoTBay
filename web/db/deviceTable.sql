86ef8f9270c791025c493400e13918ead327ab0b
STKKKKK <43334005+STKKKKK@users.noreply.github.com>
2020-06-09 01:10:09 +0800

    Update deviceTable.sql

--- web/db/deviceTable.sql
+++ web/db/deviceTable.sql
@@ -18,7 +18,25 @@ INSERT INTO DEVICES (NAME, PRICE, TYPE, STOCK)
 VALUES
 ('macbook Air', 1140.99, 'laptop', 4000),
 ('macbook Pro', 1580.99, 'laptop', 3500),
-('razor', 40.99, 'mouse', 8500);
+('macbook ProX', 1770.99, 'laptop', 1500),
+('macbook ProG', 1650.99, 'laptop', 7500),
+('macbook ProG3', 1588.49, 'laptop', 5500),
+('macbook AirF', 1260.99, 'laptop', 2500),
+('macbook AirF2', 1310.49, 'laptop', 800),
+('iphone 6', 980.99, 'phone', 3500),
+('iphone 7', 1180.99, 'phone', 4500),
+('iphone 8', 1380.99, 'phone', 4400),
+('iphone 9', 1350.99, 'phone', 3200),
+('iphone 10', 1580.49, 'phone', 7100),
+('iphone 10s', 1545.99, 'phone', 6650),
+('iphone X', 1790.49, 'phone', 7300),
+('iphone 11', 1920.99, 'phone', 5500),
+('mach S1', 580.99, 'toy', 400),
+('mach S2', 345.99, 'toy', 750),
+('mach S3', 660.99, 'toy', 1100),
+('control H11', 4255.99, 'toy', 2400),
+('control J9', 8870.99, 'toy', 550),
+('control K2', 5750.99, 'toy', 1950);
 
 SELECT * FROM DEVICES;
 
