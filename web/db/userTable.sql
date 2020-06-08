136aa647216ff5b80b793402de909803ee8a4970
STKKKKK <43334005+STKKKKK@users.noreply.github.com>
2020-06-08 20:53:20 +0800

    Update userTable.sql
    Sql typo!!

--- web/db/userTable.sql
+++ web/db/userTable.sql
@@ -2,13 +2,12 @@
  * Author:  apple
  * Created: 2020-6-2
  */
-
---DROP TABLE USERS;
+DROP TABLE USERS;
 
 CREATE TABLE USERS
 (
 Email varchar(50),
-Name varchar(10),
+Name varchar(20),
 Password varchar(20),
 Gender varchar(10),
 Favcol varchar(10),
@@ -19,8 +18,8 @@ PRIMARY KEY(Email));
 INSERT INTO USERS (EMAIL, NAME, PASSWORD, GENDER, FAVCOL, DOB, PERMISSION)
 VALUES
 ('hong@uts.edu.au', 'Hong Kung', '112233', 'male', 'Red', '1997-04-08', 'staff'),
-('xu@uts.edu.au', 'George Xu', '123456', 'male', 'Blue', '1998-05-21', 'customer');
-('feli@uts.edu.au', 'Felipe Macedo', '234567', 'male', 'green', '1998-01-21', 'customer')
+('xu@uts.edu.au', 'George Xu', '123456', 'male', 'Blue', '1998-05-21', 'customer'),
+('feli@uts.edu.au', 'Felipe Macedo', '234567', 'male', 'green', '1998-01-21', 'customer');
 
 
 SELECT * FROM USERS;
